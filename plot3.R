85b22e627ca9bad01d2705914240e9d3711380f1
Victor Naidu <Victor Naidu>
2016-07-17 01:54:09 -0700

    Add ggplot to answer question #3 and output it to file ('plot3.png')

--- plot3.R
+++ plot3.R
@@ -1,8 +1,15 @@
 source("dataHelper.R")
+library(ggplot2)
+library(dplyr)
 
 NEI  <- LoadData("summarySCC_PM25")
 SCC  <- LoadData("Source_Classification_Code")
 
-png(filename = "plot3.png",
-    width = 480, height = 480,
-    units = "px")
+NEI %>%
+  filter(fips == "24510") %>%
+  ggplot(aes(year, Emissions, color = type)) +
+  geom_line(stat = "summary", fun.y = "sum") +
+  ggtitle("Total Emissions in Baltimore City (1999 to 2008)") +
+  xlab("Year") + ylab(expression("PM"[2.5]*" Emissions"))
+
+ggsave("plot3.png", width = 5, height = 5)
