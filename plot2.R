950d068cb7e32eeeecf765e60cf3f557917d4870
Victor Naidu <Victor Naidu>
2016-07-17 00:53:57 -0700

    Append question #1 solution to script.
    As per project instructions, the script outputs plot to file (plot2.png) using the PNG format. See README.md for additional details.

--- plot2.R
+++ plot2.R
@@ -6,3 +6,15 @@ SCC  <- LoadData('Source_Classification_Code')
 png(filename = "plot2.png",
     width = 480, height = 480,
     units = "px")
+
+yrlyBaltimoreEmissions <- NEI %>%
+  filter(fips == "24510") %>%
+  select(Emissions, year) %>%
+  group_by(year) %>%
+  summarise(total = sum(Emissions))
+
+plot(yrlyBaltimoreEmissions, type = "l", xlab = "Year",
+     main = "Total Emissions in Baltimore, Maryland (1999 to 2008)",
+     ylab = expression("PM"[2.5]*" Emissions"))
+
+dev.off()
