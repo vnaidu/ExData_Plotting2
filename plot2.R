71da727737a4315519db1e02f1fd545adc990b54
Victor Naidu <Victor Naidu>
2016-07-17 01:40:57 -0700

    Rearrange code for clarity

--- plot2.R
+++ plot2.R
@@ -3,15 +3,15 @@ source("dataHelper.R")
 NEI  <- LoadData("summarySCC_PM25")
 SCC  <- LoadData("Source_Classification_Code")
 
-png(filename = "plot2.png",
-    width = 480, height = 480,
-    units = "px")
-
 yrlyBaltimoreEmissions <- NEI %>%
   filter(fips == "24510") %>%
   select(Emissions, year) %>%
   group_by(year) %>%
   summarise(total = sum(Emissions))
+
+png(filename = "plot2.png",
+    width = 480, height = 480,
+    units = "px")
 
 plot(yrlyBaltimoreEmissions, type = "l", xlab = "Year",
      main = "Total Emissions in Baltimore, Maryland (1999 to 2008)",
