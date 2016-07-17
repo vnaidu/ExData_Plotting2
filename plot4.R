7a49df10fa0fcaec42cba6b386bf7948bc238514
Victor Naidu <Victor Naidu>
2016-07-17 03:45:05 -0700

    Append solution to question #4
    Solution:
    (1) search for 'coal' in $Short.Name var of Source_Classification_Code.RDS
    (2) subset $SCC var of Source_Classification_Code.RDS where Step 1 is TRUE
    (3) subset summarySCC_PM25.RDS dataset where values in its var $SCC  appears in the resultant vector from Step 2
    (4) select vars $Emissions and $year from prev result and group by $year var
    (5) take the sum() of each group from Step 4
    (6) plot resultant using the ggplot2 library

--- plot4.R
+++ plot4.R
@@ -1,8 +1,23 @@
 source("dataHelper.R")
+library(ggplot2)
+library(dplyr)
 
-NEI  <- LoadData("summarySCC_PM25")
-SCC  <- LoadData("Source_Classification_Code")
+NEI  <- LoadData('summarySCC_PM25')
+SCC  <- LoadData('Source_Classification_Code')
 
-png(filename = "plot4.png",
-    width = 480, height = 480,
-    units = "px")
+SCC.coalInShortName <-
+  subset(SCC$SCC, grepl("coal", SCC$Short.Name, ignore.case = T))
+
+coalEmissions <-
+  NEI %>%
+  subset(NEI$SCC %in% SCC.coalInShortName) %>%
+  select(Emissions, year) %>%
+  group_by(year) %>%
+  summarise(total = sum(Emissions))
+
+ggplot(coalEmissions, aes(year, total)) +
+  geom_line() +
+  ggtitle("Total Emissions From Coal Combustion (1999 to 2008)") +
+  xlab("Year") + ylab(expression("PM"[2.5]*"Emissions"))
+
+ggsave("plot4.png", width = 5, height = 5)
