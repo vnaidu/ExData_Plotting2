6a93d6b509aa23db39d1c4b878348e1fb3817a4f
Victor Naidu <Victor Naidu>
2016-07-17 04:07:12 -0700

    Append solution to question #5

--- plot5.R
+++ plot5.R
@@ -1,8 +1,24 @@
 source("dataHelper.R")
+library(ggplot2)
+library(dplyr)
 
-NEI  <- LoadData("summarySCC_PM25")
-SCC  <- LoadData("Source_Classification_Code")
+NEI  <- LoadData('summarySCC_PM25')
+SCC  <- LoadData('Source_Classification_Code')
 
-png(filename = "plot5.png",
-    width = 480, height = 480,
-    units = "px")
+SCC.motorInShortName <-
+  subset(SCC$SCC, grepl("motor", SCC$Short.Name, ignore.case = T))
+
+motorEmissions <-
+  NEI %>%
+  subset(NEI$SCC %in% SCC.motorInShortName) %>%
+  filter(fips == "24510") %>%
+  select(Emissions, year) %>%
+  group_by(year) %>%
+  summarise(total = sum(Emissions))
+
+ggplot(motorEmissions, aes(year, total)) +
+  geom_line() +
+  ggtitle("Total Emissions from Motor Vehicle Sources\n Maryland, MD (1999-2008)") +
+  xlab("Year") + ylab(expression("PM"[2.5]*"Emissions"))
+
+ggsave("plot5.png", width = 5, height = 5)
