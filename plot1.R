5ba6b0cd5d30af86076a937d28c88fd2fe42ec68
Victor Naidu <Victor Naidu>
2016-07-17 00:18:31 -0700

    Update script to append solution to question #1
    See README.md for additional details. As per project instructions, the script outputs plot to file (plot1.png) using the PNG format.

--- plot1.R
+++ plot1.R
@@ -3,3 +3,16 @@ source("dataHelper.R")
 NEI  <- LoadData('summarySCC_PM25')
 SCC  <- LoadData('Source_Classification_Code')
 
+png(filename = "plot1.png",
+    width = 480, height = 480,
+    units = "px")
+
+yearlyEmissions <- NEI %>%
+  select(Emissions, year) %>%
+  group_by(year) %>%
+  summarise(total = sum(Emissions))
+
+plot(yearlyEmissions, type = "l", xlab = "Year", ylab = expression('PM'[2.5]*" Emissions"),
+     main = "Total Emissions in the United States (1999 to 2008)")
+
+dev.off()
