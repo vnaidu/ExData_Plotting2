4e430fdba8b45958c4a531e2784965663e1f5ad2
Victor Naidu <Victor Naidu>
2016-07-17 01:07:45 -0700

    Initial commit
    These scripts answer questions #4, #5 and #6. All require 'datahelper.R' and output plots to PNG files.

---
+++ plot4.R
@@ -0,0 +1,8 @@
+source("dataHelper.R")
+
+NEI  <- LoadData("summarySCC_PM25")
+SCC  <- LoadData("Source_Classification_Code")
+
+png(filename = "plot4.png",
+    width = 480, height = 480,
+    units = "px")
---
+++ plot6.R
@@ -0,0 +1,8 @@
+source("dataHelper.R")
+
+NEI  <- LoadData("summarySCC_PM25")
+SCC  <- LoadData("Source_Classification_Code")
+
+png(filename = "plot6.png",
+    width = 480, height = 480,
+    units = "px")
