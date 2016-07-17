3bbc974e678f0c21e23bb4ef5cb93050e16c6456
Victor Naidu <Victor Naidu>
2016-07-17 01:03:44 -0700

    Change quote style (' -> ") for consistency

--- plot3.R
+++ plot3.R
@@ -1,7 +1,7 @@
 source("dataHelper.R")
 
-NEI  <- LoadData('summarySCC_PM25')
-SCC  <- LoadData('Source_Classification_Code')
+NEI  <- LoadData("summarySCC_PM25")
+SCC  <- LoadData("Source_Classification_Code")
 
 png(filename = "plot3.png",
     width = 480, height = 480,
